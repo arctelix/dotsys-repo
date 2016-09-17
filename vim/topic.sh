2678c545e2817fc4d076d5619c9b4ec34f36f431
arctelix <arctelix@gmail.com>
2016-09-17 18:55:39 -0400

    updated vim

--- vim/topic.sh
+++ vim/topic.sh
@@ -1,5 +1,15 @@
 install () {
+    # Install vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+    # Incstall pluvins with vundle
     vim +PluginInstall +qall
+
+    # Compile you complete me
+    cd ~/.vim/bundle/YouCompleteMe
+    ./install.py --tern-completer
+
+
 }
 
 
@@ -11,4 +21,6 @@ upgrade () {
 update () {
     vim +PluginClean +qall
 }
+
+"$@"
 
