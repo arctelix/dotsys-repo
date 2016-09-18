a94b12db071de6295d756c7d16f011bbf384b235
arctelix <arctelix@gmail.com>
2016-09-18 01:42:48 -0400

    added powerline as topic & vim mods

--- powerline/topic.sh
+++ powerline/topic.sh
@@ -3,10 +3,19 @@
 install () {
 
     #install fonts  (DejaVu, Hack, Roboto seem to work best)
-    curl -L https://github.com/powerline/fonts/raw/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_DejaVu.ttf
-    curl -L https://github.com/powerline/fonts/raw/master/Hack/Hack-Regular.ttf -o ~/Library/Fonts/Powerline_Hack.ttf
-    curl -L https://github.com/powerline/fonts/raw/master/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_Droid.ttf
-    curl -L https://github.com/powerline/fonts/raw/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_Roboto.ttf
-    return $?
+    [ ! ~/Library/Fonts/Powerline_DejaVu.ttf ] && curl -L https://github.com/powerline/fonts/raw/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_DejaVu.ttf
+    [ ! ~/Library/Fonts/Powerline_Hack.ttf ] && curl -L https://github.com/powerline/fonts/raw/master/Hack/Hack-Regular.ttf -o ~/Library/Fonts/Powerline_Hack.ttf
+    [ ! ~/Library/Fonts/Powerline_Droid.ttf ] && curl -L https://github.com/powerline/fonts/raw/master/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_Droid.ttf
+    [ ! ~/Library/Fonts/Powerline_Roboto.ttf ] && curl -L https://github.com/powerline/fonts/raw/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf -o ~/Library/Fonts/Powerline_Roboto.ttf
+
+    if [ ~/Library/Fonts/Powerline_DejaVu.ttf ]; then
+        success "installed powerline fonts : DejaVu, Hack, Droid, Roboto"
+        info "You will need to select a 'for powerline' font in your terminal preferences"
+        return 0
+    else
+        return 1
+    fi
 
 }
+
+"$@"
--- vim/topic.sh
+++ vim/topic.sh
@@ -1,14 +1,21 @@
 install () {
 
     # Install vundle
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    local vundle="~/.vim/bundle/Vundle.vim"
+    [ ! "$vundle" ] && git clone https://github.com/VundleVim/Vundle.vim.git "$vundle"
 
     # Install plugins with vundle
     vim +PluginInstall +qall
 
     # Compile you complete me
     cd ~/.vim/bundle/YouCompleteMe
-    ./install.py --tern-completer
+    [ ! -f compiled.dotsys ] && ./install.py --tern-completer
+    if [ $? -eq 0 ]; then
+        touch compiled.dotsys
+        info "If you need to recompile YouCompleteMe you will have to
+      $spacer manually run ~/.vim/bundel/YouCompleteMe/install.py"
+    fi
+
 
 }
 
